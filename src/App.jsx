ab6d893e1ccd7536cd89146febb39cbc31c9b0f6
Aaron Bales <amfbales00147@usep.edu.ph>
2025-09-04 09:23:46 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen hover-slow">
+      <img src="/5.png" alt="Taste of Tales Logo" className="h-[50rem] w-auto" />
+  <button className="mt-12 w-32 h-32 rounded-full bg-amber-700 text-xl font-bold shadow-lg hover:bg-amber-800 transition-all duration-200 flex items-center justify-center sip-btn">
+        Take a sip
+      </button>
+    </div>
+  )
+}
+
+export default App
